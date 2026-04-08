cf9b345b8967ba2fdf1dcde52164f4bf6bfcb993
Msp030909 <Macorrea0309@gmail.com>
2026-04-08 09:12:59 -0700

    Enemy update 2

--- SuperDuperPlatformer/scenes/goomba.gd
+++ SuperDuperPlatformer/scenes/goomba.gd
@@ -1,6 +1,6 @@
-extends AnimatableBody2D
+extends CharacterBody2D
 @onready var rayCast: RayCast2D = $RayCast2D
-@export var direction := true
+@export var direction := 1
 var speed = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,13 +13,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if rayCast.is_colliding() == true:
 		if rayCast.get_collider() is not CharacterBody2D:
-			direction = not direction
+			direction = -direction
 			print("direction should be switched now")
 			print(direction)
 			rayCast.scale *= -1
 			$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
-	if direction == false:
-		position += Vector2(speed,0)
-	if direction == true:
-		position -= Vector2(speed,0)
+	if is_on_floor():
+		position -= Vector2(speed * direction,0)
+	elif not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	move_and_slide()
 	pass
