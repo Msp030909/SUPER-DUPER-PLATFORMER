15986733b40f986ee642530920e9aa9eea35102a
Msp030909 <Macorrea0309@gmail.com>
2026-04-28 15:04:50 -0700

    new soup
    this is broken pls fix

--- SuperDuperPlatformer/scenes/angle_finder.gd
+++ SuperDuperPlatformer/scenes/angle_finder.gd
@@ -1,7 +1,8 @@
 extends Node2D
 var ypos
 var xpos
-enum angles { LEFT, RIGHT, UP, DOWN }
+var dir
+enum angles { FORWARD, BACKWARD, UP, DOWN }
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,17 +12,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var angle = atan2(get_global_mouse_position().y,get_global_mouse_position().x)
-	rotation = angle
-	if get_angle_to(get_global_mouse_position()) <= 0:
-		ypos = true
-	else:
-		ypos = false
+	rotation = lerp_angle(angle,rotation, delta)
 	var rangle = round(angle)
 	print(rangle)
-	if rangle >= 0 and rangle <= 1:
+	if rangle == 0:
+		dir = angles.FORWARD
 		print("forwards")
 	elif rangle <= -1 and rangle >= -2:
+		dir = angles.UP
 		print("up")
-	elif rangle >= 1 or rangle <= -3:
+	elif rangle >= 3 or rangle <= -3:
+		dir = angles.BACKWARD
 		print("back")
+	else:
+		dir = angles.DOWN
+		print("down")
 	pass
