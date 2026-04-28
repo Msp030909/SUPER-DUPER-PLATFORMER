0fcafe0427056eaaf238676868d53a7210b92775
Msp030909 <Macorrea0309@gmail.com>
2026-04-28 14:51:42 -0700

    THE ANGLE FINDER IS REAL

--- SuperDuperPlatformer/scenes/angle_finder.gd
+++ SuperDuperPlatformer/scenes/angle_finder.gd
@@ -1,5 +1,7 @@
 extends Node2D
-
+var ypos
+var xpos
+enum angles { LEFT, RIGHT, UP, DOWN }
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,5 +10,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotation = atan2(get_global_mouse_position().y,get_global_mouse_position().x)
+	var angle = atan2(get_global_mouse_position().y,get_global_mouse_position().x)
+	rotation = angle
+	if get_angle_to(get_global_mouse_position()) <= 0:
+		ypos = true
+	else:
+		ypos = false
+	var rangle = round(angle)
+	print(rangle)
+	if rangle >= 0 and rangle <= 1:
+		print("forwards")
+	elif rangle <= -1 and rangle >= -2:
+		print("up")
+	elif rangle >= 1 or rangle <= -3:
+		print("back")
 	pass
