b85dfd9df479ae5136852d151e1ff8cc3f833555
Msp030909 <Macorrea0309@gmail.com>
2026-06-02 15:01:44 -0700

    Arena mode now has PORTALS

--- SuperDuperPlatformer/scenes/components/portal_component.gd
+++ SuperDuperPlatformer/scenes/components/portal_component.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name Portal
 @export var destination: Portal
+@export var dir: int
+var justTped = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,9 +11,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if justTped == true:
+		await get_tree().create_timer(1)
+		justTped = false
 	pass
 
 
 func _on_body_entered(body: ArenaPlayer) -> void:
-	body.position.x = destination.position.x
+	
+	if justTped != true:
+		body.position.x = destination.position.x + 20 * dir
+		destination.justTped = true
 	pass # Replace with function body.
